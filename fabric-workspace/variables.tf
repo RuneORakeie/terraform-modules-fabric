030ce39d40d1cfbc7f004e2423867ac3e3b6334b
Rune Ovlien Rakeie <rune.rakeie@tietoevry.com>
2024-12-18 11:57:30 +0100

    Fixed typo in var validation

--- fabric-workspace/variables.tf
+++ fabric-workspace/variables.tf
@@ -24,28 +24,28 @@ variable "role_assignment_list" {
   }))
   validation {
     condition = alltrue([
-      for assignment in var.role_assignment_map :
+      for assignment in var.role_assignment_list :
       can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$", assignment.principal_id))
     ])
     error_message = "Principal ID must be a valid UUID format."
   }
   validation {
     condition = alltrue([
-      for assignment in var.role_assignment_map :
+      for assignment in var.role_assignment_list :
       contains(["Group", "ServicePrincipal", "ServicePrincipalProfile", "User"], assignment.principal_type)
     ])
     error_message = "Principal type must be one of: Group, ServicePrincipal, ServicePrincipalProfile, User."
   }
   validation {
     condition = alltrue([
-      for assignment in var.role_assignment_map :
+      for assignment in var.role_assignment_list :
       contains(["Admin", "Contributor", "Member", "Viewer"], assignment.role)
     ])
     error_message = "Role must be one of: Admin, Contributor, Member, Viewer."
   }
   validation {
-    condition = length(distinct([for assignment in var.role_assignment_map :
-    "${assignment.principal_id}-${assignment.role}"])) == length(var.role_assignment_map)
+    condition = length(distinct([for assignment in var.role_assignment_list :
+    "${assignment.principal_id}-${assignment.role}"])) == length(var.role_assignment_list)
     error_message = "Duplicate role assignments for the same principal are not allowed."
   }
 }
